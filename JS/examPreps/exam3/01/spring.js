d0b99db1b25797aebc57cc3ab1c5077e24e4c57e
barishm <barishm@outlook.com>
2023-04-18 15:00:11 +0300

    exam preps

--- JS/examPreps/exam3/01/spring.js
+++ JS/examPreps/exam3/01/spring.js
@@ -1,7 +1,12 @@
 function solve(input) {
     let n = Number(input.shift());
-    let springBoard = {};
-    let tasks ={};
+    let springBoard = [];
+    let tasks = [];
+    let task = {};
+    let todoSum = 0;
+    let inProgressSum = 0;
+    let codeReviewSum = 0;
+    let donePointsSum = 0;
     let commandsParser = {
         'Add New': addTask,
         'Remove Task': removeTask,
@@ -9,15 +14,17 @@ function solve(input) {
     };
     for (let index = 0; index < n; index++) {
         let [assignee,taskId,title,status,eP] = input.shift().split(':');
-        tasks[taskId] = {title,status,eP};
-        if(springBoard.hasOwnProperty(assignee)){
-            springBoard[assignee][taskId] = {title,status,eP}; 
+        if(springBoard.hasOwnProperty(assignee)) {
+            task = {taskId,title,status,eP};
+            springBoard[assignee].push(task);
         }else {
+            task = {taskId,title,status,eP};
+            tasks.push(task);
             springBoard[assignee] = tasks;
         }
-        
-
-        tasks = {};
+        assignee = {};
+        task = {};
+        tasks = [];
     }
 
     for (const inputLine of input) {
@@ -27,36 +34,70 @@ function solve(input) {
         
     }
     
-    for (const piece in piecesCollection) {
-        const {key,composer} = piecesCollection[piece];
-        console.log(`${piece} -> Composer: ${composer}, Key: ${key}`)
+    for (const assignee in springBoard) {
+        for (let index = 0; index < springBoard[assignee].length; index++) {
+            if(springBoard[assignee][index].status === "ToDo"){
+                todoSum += Number(springBoard[assignee][index].eP);
+            }
+            if(springBoard[assignee][index].status === "In Progress"){
+                inProgressSum += Number(springBoard[assignee][index].eP);
+            }
+            if(springBoard[assignee][index].status === "Code Review"){
+                codeReviewSum += Number(springBoard[assignee][index].eP);
+            }
+            if(springBoard[assignee][index].status === "Done"){
+                donePointsSum += Number(springBoard[assignee][index].eP);
+            }
+        }
     }
+    console.log(`ToDo: ${todoSum}pts`);
+    console.log(`In Progress: ${inProgressSum}pts`);
+    console.log(`Code Review: ${codeReviewSum}pts`);
+    console.log(`Done Points: ${donePointsSum}pts`);
+    if(donePointsSum >= todoSum + codeReviewSum + inProgressSum){
+        console.log("Sprint was successful!");
+    }else {
+        console.log("Sprint was unsuccessful...");
+    }
+
 
     function addTask(assignee,taskId,title,status,eP){
         if(springBoard.hasOwnProperty(assignee)){
-            
-            springBoard[assignee][taskId] = {title,status,eP}; 
-            tasks = {};
+            task = {taskId,title,status,eP};
+            springBoard[assignee].push(task);
         }else {
-            
+            console.log(`Assignee ${assignee} does not exist on the board!`);
         }
     }
 
     function removeTask(assignee,index){
         if(springBoard.hasOwnProperty(assignee)){
-            delete springBoard[assignee][index];
+            if(index < 0 || index >= springBoard[assignee].length){
+                console.log(`Index is out of range!`);
+            }else {
+                springBoard[assignee].splice(index,1);
+                
+            }
         }else {
-            console.log(`Invalid operation! ${piece} does not exist in the collection.`);
+            console.log(`Assignee ${assignee} does not exist on the board!`);
         }
 
     }
 
-    function changeStatus(piece,newKey){
-        if(piecesCollection.hasOwnProperty(piece)){
-            piecesCollection[piece].key = newKey;
-            console.log(`Changed the key of ${piece} to ${newKey}!`);
+    function changeStatus(assignee,taskId,newStatus){
+        if(springBoard.hasOwnProperty(assignee)){
+            let length = springBoard[assignee].length;
+            for (let index = 0; index < length; index++) {
+                if(springBoard[assignee][index].taskId === taskId){
+                    springBoard[assignee][index].status = newStatus
+                }
+                else {
+                    console.log(`Task with ID ${taskId} does not exist for ${assignee}!`);
+                }
+                
+            }
         }else {
-            console.log(`Invalid operation! ${piece} does not exist in the collection.`);
+            console.log(`Assignee ${assignee} does not exist on the board!`);
         }
 
     }
@@ -71,6 +112,7 @@ solve([
     'Georgi:BOP-1212:Investigation Task:Done:2',
     'Mariya:BOP-1213:New Account Page:In Progress:13',
     'Add New:Kiril:BOP-1217:Add Info Page:In Progress:5',
-    'Remove Task:Mariya:1',
+    'Change Status:Peter:BOP-1290:ToDo',
+    'Remove Task:Mariya:0',
     'Remove Task:Joro:1',
 ]);
